29b346ff4b91717593fc2d7bce6ca027d6a4948f
achim <achimunterkofler@gmx.at>
2022-07-20 15:47:26 +0200

    mutation funktioniert

--- src/main/kotlin/graph/ql/SampleMutation.kt
+++ src/main/kotlin/graph/ql/SampleMutation.kt
@@ -4,8 +4,9 @@ import jakarta.inject.Singleton
 
 @Singleton
 class SampleMutation {
-
+    var testRepoForString: TestRepoForString = TestRepoForString()
     fun createString(aString: String):String{
+        testRepoForString.creatString(aString)
         return "OK"
     }
 }
